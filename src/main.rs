9c93ff644c5c917960c96131c5372789290cce43
JP Verkamp <jverkamp@dropbox.com>
2024-08-30 11:09:06 -0400

    Initial command line setup for tokenize

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,39 @@
-use std::env;
-use std::fs;
-use std::io::{self, Write};
+use std::{fs, path::PathBuf};
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        writeln!(io::stderr(), "Usage: {} tokenize <filename>", args[0]).unwrap();
-        return;
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+
+/// Implementation of the lox programming language for code crafters
+#[derive(Debug, Parser)]
+#[clap(name = "jp-lox", version)]
+pub struct Args {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    /// Tokenize the input file.
+    Tokenize {
+        /// Input lox file. 
+        path: PathBuf,
+    },
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    match args.command {
+        Command::Tokenize { path } => {
+            let file_contents = fs::read_to_string(&path)?;
+
+            if !file_contents.is_empty() {
+                unimplemented!("Tokenize the input file");
+            } else {
+                println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
+            }
+        },
     }
 
-    let command = &args[1];
-    let filename = &args[2];
-
-    match command.as_str() {
-        "tokenize" => {
-            // You can use print statements as follows for debugging, they'll be visible when running tests.
-            writeln!(io::stderr(), "Logs from your program will appear here!").unwrap();
-
-            let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
-                writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
-                String::new()
-            });
-
-            // Uncomment this block to pass the first stage
-            // if !file_contents.is_empty() {
-            //     panic!("Scanner not implemented");
-            // } else {
-            //     println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
-            // }
-        }
-        _ => {
-            writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
-            return;
-        }
-    }
+    Ok(())
 }
